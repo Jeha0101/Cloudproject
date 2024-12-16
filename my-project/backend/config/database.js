51de35bd991cb06dd39ea85c73d37f584baac178
Jeha0101 <155410554+Jeha0101@users.noreply.github.com>
2024-12-17 02:11:59 +0900

    Backend 환경변수 얻어오기 수정

--- my-project/backend/config/database.js
+++ my-project/backend/config/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'database',
-    user: 'user',
-    password: 'userpassword',
-    database: 'mydb',
+    host: process.env.DATABASE_HOST || 'database',
+    user: process.env.MYSQL_USER || 'user',
+    password: process.env.MYSQL_PASSWORD || 'userpassword',
+    database: process.env.MYSQL_DATABASE || 'mydb',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
